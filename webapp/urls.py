01da233fd51c655aca694bd18235d2fc357ebe9a
Phuc Le-Sanh <phucls288@gmail.com>
2016-10-20 14:24:04 +0800

    [GUI] Implement log in page

--- webapp/urls.py
+++ webapp/urls.py
@@ -7,10 +7,11 @@
 # Modified by:    Phuc Le-Sanh
 """
 
-from django.conf.urls import url
+from django.conf.urls import url, include
 
 from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url('^', include('django.contrib.auth.urls')),
 ]
