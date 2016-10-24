57479f3183436717c4f97f7706f652a654abc80e
Phuc Le-Sanh <phucls288@gmail.com>
2016-10-24 12:53:54 +0800

    Fix en.js

--- webapp/static/webapp/js/i18n/en.js
+++ webapp/static/webapp/js/i18n/en.js
@@ -2,7 +2,7 @@ const en = {
     title: {
         login: "Mathematics Learning System - Sign In",
         dashboard: "Mathematics Learning System - Dashboard",
-        adaptive_test: "Mathematics Learning System - Adaptive Test"
+        adaptive_test: "Mathematics Learning System - Adaptive Test",
         contest: "Mathematics Learning System - Contest"
     }
 };
