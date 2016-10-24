7ffddf924b6e74955fd76f943e7448c0c11f02be
Phuc Le-Sanh <phucls288@gmail.com>
2016-10-24 14:32:26 +0800

    [REST] Implement permissions

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,13 @@
-from . import views
+from api import views
 
 from django.conf.urls import url, include
 from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework.authtoken import views as rest_views
 
 urlpatterns = [
     url(r'^api-auth/', include('rest_framework.urls',
                                namespace='rest_framework')),
+    url(r'^api-token-auth/', rest_views.obtain_auth_token),
 
     url(r'^users/$', views.UserList.as_view()),
     url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
