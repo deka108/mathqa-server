37fa4b9c4dcc09564164518d642d62f421cf6c01
Phuc Le-Sanh <phucls288@gmail.com>
2016-10-29 20:33:38 +0800

    Revert Django Admin to default

--- webapp/admin.py
+++ webapp/admin.py
@@ -4,20 +4,44 @@ from django.contrib.admin import ModelAdmin
 from meas_models.models import *
 
 
+class EducationLevelAdmin(ModelAdmin):
+    list_display = ('id', 'name', 'description',)
+    list_editable = ('name', 'description',)
+
+
+admin.site.register(EducationLevel, EducationLevelAdmin)
+
+
+class SubjectAdmin(ModelAdmin):
+    list_display = ('id', 'name', 'description',)
+    list_editable = ('name', 'description',)
+
+
+admin.site.register(Subject, SubjectAdmin)
+
+
 class TopicAdmin(ModelAdmin):
-    list_display = ('id', 'name', 'description', 'order')
-    list_editable = ('name', 'description', 'order')
+    list_display = ('id', 'name', 'description', 'order',)
+    list_editable = ('name', 'description', 'order',)
 
 
 admin.site.register(Topic, TopicAdmin)
 
 
 class ConceptAdmin(ModelAdmin):
-    list_display = ('id', 'name', 'description')
-    list_editable = ('name', 'description')
+    list_display = ('id', 'name', 'description',)
+    list_editable = ('name', 'description',)
 
 
 admin.site.register(Concept, ConceptAdmin)
+
+
+class TestAdmin(ModelAdmin):
+    list_display = ('id', 'name', 'test_type')
+    list_editable = ('name', 'test_type')
+
+
+admin.site.register(Test, TestAdmin)
 
 
 class QuestionAdmin(ModelAdmin):
