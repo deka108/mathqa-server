77c3f424798c74515fe03183b12a4e1b25dcd5fc
Phuc Le-Sanh <phucls288@gmail.com>
2016-11-24 18:01:40 +0800

    [HotFix] Sprint Nov 2016

--- webapp/static/webapp/js/topic/index.js
+++ webapp/static/webapp/js/topic/index.js
@@ -1,7 +1,14 @@
 var MQ = MathQuill.getInterface(2);
 
 $(document).ready(function() {
-    $("#items").DataTable();
+    $('#items').DataTable({
+        "drawCallback": function( settings ) {
+        // MathJax.Hub.Config({
+        //    tex2jax: {inlineMath: [["$","$"],["\\(","\\)"]]}
+        // });
+        MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
+        }
+    });
     // Breadcum
     $('#breadcrumb-home').append(en.cms.breadcrumb.home);
     $('#breadcrumb-question').append(en.cms.breadcrumb.question);
