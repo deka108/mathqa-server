8e728540174f3c0f20e630452e7b312f1eb27974
Phuc Le-Sanh <phucls288@gmail.com>
2016-11-30 15:17:38 +0800

    [Demo-End_Nov2016] Add collapsible solution to question in webapp

--- webapp/views.py
+++ webapp/views.py
@@ -61,9 +61,9 @@ def topic_concept(request, topic_id, concept_id):
     topic = Topic.objects.get(pk=topic_id)
     concept = Concept.objects.get(pk=concept_id)
     keypoints = concept.keypoint_set.all()
-    questions = concept.question_set.filter(keypoint__isnull=True,
-                                            paper__isnull=False,
-                                            paper__month="11")
+    questions = concept.question_set.filter(keypoint__isnull=True)
+    # paper__isnull=False,
+    # paper__month="11")
     k_questions = Question.objects.filter(keypoint__in=keypoints)
 
     return render(request, 'webapp/topic/index.html', __user_info(request, {
