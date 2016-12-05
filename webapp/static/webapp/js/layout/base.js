50196be121b5bf3ee6bb7a840e326624524b52e2
Phuc Le-Sanh <phucls288@gmail.com>
2016-12-05 23:20:19 +0800

    [Release_1] Add search interface to Dashboard

--- webapp/static/webapp/js/layout/base.js
+++ webapp/static/webapp/js/layout/base.js
@@ -13,4 +13,55 @@ $( document ).ready(function() {
     $("#sign-out").append(en.menu.sign_out);
     $("#register").append(en.menu.register);
     $("#login").append(en.menu.login);
+
+    $("#search-box").attr("onkeyup", "Preview.Update()");
+
+    $("#btn-1").click(function(){
+        $("#search-box").val($("#search-box").val() + "\\frac{x}{y}");
+    });
+    $("#btn-2").click(function(){
+        $("#search-box").val($("#search-box").val() + "e^x");
+    });
+    $("#btn-3").click(function(){
+        $("#search-box").val($("#search-box").val() + "x^2");
+    });
+    $("#btn-4").click(function(){
+        $("#search-box").val($("#search-box").val() + "{x}_{2}");
+    });
+    $("#btn-5").click(function(){
+        $("#search-box").val($("#search-box").val() + "\\sqrt{x}");
+    });
+    $("#btn-6").click(function(){
+        $("#search-box").val($("#search-box").val() + "\\sqrt[3]{x}");
+    });
+    $("#btn-7").click(function(){
+        $("#search-box").val($("#search-box").val() + "\\int_{-1}^{1} xdx");
+    });
+    $("#btn-8").click(function(){
+        $("#search-box").val($("#search-box").val() + "\\sum_{i=0}^{n} {a}_{n}");
+    });
+    $("#btn-9").click(function(){
+        $("#search-box").val($("#search-box").val() + "\\overrightarrow{AB}");
+    });
+    $("#btn-10").click(function(){
+        $("#search-box").val($("#search-box").val() + "\\sin x");
+    });
+    $("#btn-11").click(function(){
+        $("#search-box").val($("#search-box").val() + "\\cos x");
+    });
+    $("#btn-12").click(function(){
+        $("#search-box").val($("#search-box").val() + "\\tan x");
+    });
+    $("#btn-13").click(function(){
+        $("#search-box").val($("#search-box").val() + "\\cot x");
+    });
+    $("#btn-14").click(function(){
+        $("#search-box").val($("#search-box").val() + "\\lim_(n \to \infty)");
+    });
+    $("#btn-15").click(function(){
+        $("#search-box").val($("#search-box").val() + "\\log x");
+    });
+    $("#btn-16").click(function(){
+        $("#search-box").val($("#search-box").val() + "\\ln x");
+    });
 });
